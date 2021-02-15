e5e2dd2d735e0d41ac48d7528cbc6154f230a85c
Daniel Avila <daniel.avila.vera@outlook.com>
2021-02-15 15:53:32 -0500

    verify server vars

--- index.php
+++ index.php
@@ -13,9 +13,8 @@ MercadoPago\SDK::setIntegratorId('dev_2e4ad5dd362f11eb809d0242ac130004');
 
 $container = new Container();
 
-print_r($_SERVER);
-
-/*$container->set('url', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/');
+//REQUEST_SCHEME
+$container->set('url', $_SERVER['HTTP_X_FORWARDED_PROTO'] . '://' . $_SERVER['HTTP_HOST'] . '/');
 
 $container->set('view', function(){
     $view = new App\Util\Twig(APP_ROOT. DIRECTORY_SEPARATOR . 'templates', [
@@ -42,4 +41,4 @@ $app->get('/failure', App\Controller\AppController::class . ':failure');
 $app->post('/api/preferences', App\Controller\ApiController::class . ':preferences');
 $app->post('/api/webhook', App\Controller\ApiController::class . ':webhook');
 
-$app->run();*/
+$app->run();
